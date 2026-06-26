949ff140b8c0c13ee6de9517888b85b79e17b0a7
Daniel Parsons <parsonsdwp@gmail.com>
2026-06-26 17:30:48 +0900

    feat: initialize benchmarking main program

---
+++ experiments/benchmarking/run_benchmarks.py
@@ -0,0 +1,27 @@
+
+def run_identify_essay_benchmark(base_url, max_tokens, temp):
+    return
+
+def run_citations_benchmarks(base_url, max_tokens, temp):
+    return
+
+def run_coherence_benchmarks(base_url, max_tokens, temp):
+    return
+
+def run_conclusion_benchmarks(base_url, max_tokens, temp):
+    return
+
+def run_grammar_benchmarks(base_url, max_tokens, temp):
+    return
+
+def run_introduction_benchmarks(base_url, max_tokens, temp):
+    return
+
+def run_paragraph_benchmarks(base_url, max_tokens, temp):
+    return
+
+def run_thesis_benchmarks(base_url, max_tokens, temp):
+    return
+
+def run_vocabulary_benchmarks(base_url, max_tokens, temp):
+    return
